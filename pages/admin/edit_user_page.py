7386b3ce6c052324a581a0455c7425d8aad6c271
r0 <64012087+dominggo1999@users.noreply.github.com>
2023-03-17 12:19:46 +0700

    feat : implement admin pages

---
+++ pages/admin/edit_user_page.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class EditUserPage:
+    def __init__(self, browser: WebDriver):
+        self.browser = browser
+
+    def user_role_options(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ":nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper")
+
+    def employee_name_autocomplete(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ":nth-child(1) > .oxd-grid-2 > :nth-child(2) > .oxd-input-group .oxd-autocomplete-wrapper")
+
+    def status_options(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper")
+
+    def username_field(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ":nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-input")
+
+    def save_button(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".oxd-button--secondary")
+
+    def cancel_button(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".oxd-button--ghost")
+
+    def submit(self):
+        self.save_button().click()
+
+    def cancel(self):
+        self.cancel_button().click()
---
+++ pages/admin/users_page.py
@@ -0,0 +1,87 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from utils.create_target_url import create_target_url
+
+
+class UsersPage:
+    def __init__(self, browser: WebDriver):
+        self.browser = browser
+        self.url = create_target_url("/admin/viewSystemUsers")
+        self.displayedUserData = [
+            "Username",
+            "User Role",
+            "Employee Name",
+            "Status",
+            "Actions",
+        ]
+
+    def user_table(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".oxd-table")
+
+    def user_table_header(self):
+        return self.user_table().find_element(By.CSS_SELECTOR, ".oxd-table-header")
+
+    def user_table_body(self):
+        return self.user_table().find_element(By.CSS_SELECTOR, ".oxd-table-body")
+
+    def user_filter(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".oxd-table-filter")
+
+    def username_field(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ":nth-child(2) > .oxd-input")
+
+    def user_role_options(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper")
+
+    def employee_name_autocomplete(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".oxd-autocomplete-wrapper")
+
+    def status_field(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ":nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper")
+
+    def add_user_button(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".orangehrm-header-container > .oxd-button")
+
+    def user_table_rows(self):
+        return self.user_table().find_elements(By.CSS_SELECTOR, ".oxd-table-card")
+
+    def last_user_row(self):
+        return self.user_table_body().find_element(By.CSS_SELECTOR, ".oxd-table-card:last-child")
+
+    def last_user_username(self):
+        return self.user_table_body().find_element(By.CSS_SELECTOR, ".oxd-table-card:last-child .oxd-table-cell:nth-of-type(2)")
+
+    def delete_user_confirm_button(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".oxd-dialog-container-default--inner .oxd-button--label-danger")
+
+    def delete_user_cancel_button(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".oxd-dialog-container-default--inner .oxd-button--text")
+
+    def apply_filter_button(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".oxd-form-actions > .oxd-button--secondary")
+
+    def reset_filter_button(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".oxd-button--ghost")
+
+    def visit(self):
+        self.browser.get(self.url)
+
+    def edit_last_user_row(self):
+        self.last_user_row().find_element(
+            By.CSS_SELECTOR, ".oxd-table-cell-actions button:nth-of-type(2)").click()
+
+    def delete_last_user_row(self):
+        self.last_user_row().find_element(
+            By.CSS_SELECTOR, ".oxd-table-cell-actions button:nth-of-type(1)").click()
+
+    def cancel_delete(self):
+        self.delete_user_cancel_button().click()
+
+    def confirm_delete(self):
+        self.delete_user_confirm_button().click()
+
+    def apply_filter(self):
+        self.apply_filter_button().click()
+
+    def reset_filter(self):
+        self.reset_filter_button().click()
